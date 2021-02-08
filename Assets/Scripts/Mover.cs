676fb490beb951f04cb775626bde9afe2b4acdd7
Michael Barry <g00358789@gmit.ie>
2021-02-08 19:09:09 +0000

    2.8

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor() // Using ray casts to move about world
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //ray is shot from origin to point within main camera
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit); 
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point; //calls the position of the ray cast hit and moves to it
+        }
+    }
+}
